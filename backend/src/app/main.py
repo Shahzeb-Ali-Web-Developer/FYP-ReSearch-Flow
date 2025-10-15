527e27b5c4494e750e56af6869ac4613ee26edc2
Haroon Mahmood <148611250+haroonaulakh@users.noreply.github.com>
2025-10-15 21:11:52 +0500

    Update main.py

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://fyp-re-search-flow.vercel.app/",
+        "https://fyp-re-search-flow.vercel.app",
         "http://localhost:5173",  # Vite default
 ],
     allow_credentials=True,
